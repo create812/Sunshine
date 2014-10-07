6f747f3af2bad23548a5c434f0f97d2e76b04552
Ben Johns <create812@gmail.com>
2014-10-07 13:43:01 -0400

    Fixing ForecastFragment

--- app/src/main/java/app/sunsine/android/example/com/sunshine/ForecastFragment.java
+++ app/src/main/java/app/sunsine/android/example/com/sunshine/ForecastFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,7 +24,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * A placeholder fragment containing a simple view. testst
+ * A placeholder fragment containing a simple view.
  */
 public class ForecastFragment extends Fragment {
 
@@ -30,10 +32,27 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.forecasefragment, menu);
-        return true;
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        //Handle action bar item clicks here. The action bar will
+        //automatically handle clicks on the Home/Up button, so long
+        //as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_refresh){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
